ab8f85b2d2a31a7306dc00f158fd24c314ad2354
Aaron Ferris Richardson <aaron.richardson@gmail.com>
2021-04-07 21:22:41 +0100

    fix (Ch27): typo and import cleanup

--- src/main/java/Five_Generics/Ch27EliminateUncheckedReferences.java
+++ src/main/java/Five_Generics/Ch27EliminateUncheckedReferences.java
@@ -1,13 +1,12 @@
 package Five_Generics;
 
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 public class Ch27EliminateUncheckedReferences {
 
     public static void main(String[] args) {
-        // If suppressed a comment as to why.
+        // If suppressed add a comment as to why.
         // For instance you plan and have verified explicetely that you only use 'Lark's in exaltation.
         @SuppressWarnings("unchecked") Set<Lark> exaltation = new HashSet<>();
         System.out.println(exaltation);
